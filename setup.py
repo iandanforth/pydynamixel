4eddfc8a50c1bc32c4175bbe817fe8dd207f4e91
Ian Danforth <iandanforth@gmail.com>
2012-04-10 20:11:52 -0700

    Update setup.py for compatability with PyPi

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+__version__ = '1.0.1'
 
-setup(name = "dynamixel",
-      version = '1.0.1',
-      description = "Dynamixel Servo Library",
-      packages = [ 'dynamixel' ],
-      platforms = 'Posix; MacOS X; Windows',
-      )
+sdict = {
+    'name' : 'dynamixel',
+    'version' : __version__,
+    'description' : "Dynamixel Servo Library",
+    'url': 'http://github.com/iandanforth/pydynamixel',
+    'download_url' : 'https://github.com/iandanforth/pydynamixel/downloads',
+    'author' : 'Patrick Goebel',
+    'author_email' : 'patrick@pirobot.org',
+    'maintainer' : 'Ian Danforth',
+    'maintainer_email' : 'iandanforth@gmail.com',
+    'keywords' : ['dynamixel', 'robotis', 'ax-12'],
+    'license' : 'GPL',
+    'packages' : ['dynamixel'],
+    'classifiers' : [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python'],
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**sdict)
