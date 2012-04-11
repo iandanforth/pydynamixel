5214741999d38437b483e544960ce5b941038c9c
Ian Danforth <iandanforth@gmail.com>
2012-04-10 19:51:38 -0700

    Make initial experience more friendly. Exit if no servos are found. Also display a list of usb tty ports the user might want to use

--- example.py
+++ example.py
@@ -3,13 +3,29 @@ import dynamixel
 import time
 import random
 import sys
+import subprocess
 
-# The list of Servo Ids on our Bus
-servoList = [16]
+###############################################################################
+# The end point of the servos we will scan for.
+#
+# If you have servos with ids 3, 12, and 15 this value should be 15.
+# This will scan at approximately one servo id a second so don't worry if it
+# takes a little bit.
+
+lastServoId = 16
 
 # Set your serial port accordingly.
 if os.name == "posix":
-    portName = "/dev/tty.usbserial-A8005k21"
+  portName = None
+  if not portName:
+    print ('You will need to edit this example and set a value for "portName".\n'
+          'Here are a few possible ports you might want to try if you do not\n'
+          'know exactly where your USB2Dynamixel is attached:')
+  # Get a list of ports that mention USB
+  possiblePorts = subprocess.check_output('ls /dev/*usb*', shell=True).split()
+  for port in possiblePorts:
+    print '\t' + port
+  sys.exit(1)
 else:
     portName = "COM11"
 
@@ -18,21 +34,20 @@ baudRate = 1000000
 
 serial = dynamixel.SerialStream( port=portName, baudrate=baudRate, timeout=1)
 net = dynamixel.DynamixelNetwork( serial )
-net.scan( 1, nServos )
+net.scan( 1, lastServoId )
 
 myActuators = list()
 
 print "Scanning for Dynamixels..."
 for dyn in net.get_dynamixels():
-    print dyn.id,
+    print dyn.id
     myActuators.append(net[dyn.id])
 
 if not myActuators:
-  print "No Dynamixels Found!"
+  print 'No Dynamixels Found!'
   sys.exit(0)
 else:
   print "...Done"
-
 
 for actuator in myActuators:
     actuator.moving_speed = 50
