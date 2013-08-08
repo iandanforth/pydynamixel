1784a28c53d9d966dd94ccf817216a90e5cff7f9
Ian Danforth <iandanforth@gmail.com>
2013-08-08 15:29:44 -0700

    Make example.py more user friendly

--- example.py
+++ example.py
@@ -4,65 +4,146 @@ import time
 import random
 import sys
 import subprocess
+import optparse
+import yaml
 
-###############################################################################
-# The end point of the servos we will scan for.
-#
-# If you have servos with ids 3, 12, and 15 this value should be 15.
-# This will scan at approximately one servo id a second so don't worry if it
-# takes a little bit.
+"""
+EXAMPLE
+ 
+Move all attached servos randomly every 2 seconds and read back the position
+they end up in.
+"""
 
-lastServoId = 16
+def main(settings):
 
-# Set your serial port accordingly.
-if os.name == "posix":
-  portName = None
-  if not portName:
-    print ('You will need to edit this example and set a value for "portName".\n'
-          'Here are a few possible ports you might want to try if you do not\n'
-          'know exactly where your USB2Dynamixel is attached:')
-    # Get a list of ports that mention USB
-    possiblePorts = subprocess.check_output('ls /dev/*usb*', shell=True).split()
-    for port in possiblePorts:
-      print '\t' + port
-    sys.exit(1)
-else:
-    portName = "COM11"
+    portName = settings['port']
+    baudRate = settings['baudRate']
+    highestServoId = settings['highestServoId']
 
-# Default baud rate of the USB2Dynamixel device.
-baudRate = 1000000
-
-serial = dynamixel.SerialStream( port=portName, baudrate=baudRate, timeout=1)
-net = dynamixel.DynamixelNetwork( serial )
-net.scan( 1, lastServoId )
-
-myActuators = list()
-
-print "Scanning for Dynamixels..."
-for dyn in net.get_dynamixels():
-    print dyn.id
-    myActuators.append(net[dyn.id])
-
-if not myActuators:
-  print 'No Dynamixels Found!'
-  sys.exit(0)
-else:
-  print "...Done"
-
-for actuator in myActuators:
-    actuator.moving_speed = 50
-    actuator.synchronized = True
-    actuator.torque_enable = True
-    actuator.torque_limit = 800
-    actuator.max_torque = 800
-
-while True:
+    # Establish a serial connection to the dynamixel network.
+    # This usually requires a USB2Dynamixel
+    serial = dynamixel.SerialStream( port=portName, baudrate=baudRate, timeout=1)
+    net = dynamixel.DynamixelNetwork( serial )
+    
+    # Ping the range of servos that are attached
+    net.scan( 1, highestServoId )
+    
+    myActuators = list()
+    
+    print "Scanning for Dynamixels..."
+    for dyn in net.get_dynamixels():
+        print dyn.id
+        myActuators.append(net[dyn.id])
+    
+    if not myActuators:
+      print 'No Dynamixels Found!'
+      sys.exit(0)
+    else:
+      print "...Done"
+    
     for actuator in myActuators:
-        actuator.goal_position = random.randrange(450, 600)
-    net.synchronize()
-    for actuator in myActuators:
-        actuator.read_all()
-        time.sleep(0.01)
-    for actuator in myActuators:
-        print actuator.cache[dynamixel.defs.REGISTER['Id']], actuator.cache[dynamixel.defs.REGISTER['CurrentPosition']]
-    time.sleep(2)
+        actuator.moving_speed = 50
+        actuator.synchronized = True
+        actuator.torque_enable = True
+        actuator.torque_limit = 800
+        actuator.max_torque = 800
+    
+    while True:
+        for actuator in myActuators:
+            actuator.goal_position = random.randrange(450, 600)
+        net.synchronize()
+        for actuator in myActuators:
+            actuator.read_all()
+            time.sleep(0.01)
+        for actuator in myActuators:
+            print actuator._id, actuator.current_position
+        time.sleep(2)
+
+def validateInput(userInput, rangeMin, rangeMax):
+    '''
+    Returns valid user input or None
+    '''
+    try:
+        inTest = int(userInput)
+        if inTest < rangeMin or inTest > rangeMax:
+            print "ERROR: Value out of range [" + str(rangeMin) + '-' + str(rangeMax) + "]"
+            return None
+    except ValueError:
+        print("ERROR: Please enter an integer")
+        return None
+    
+    return inTest
+
+if __name__ == '__main__':
+    
+    parser = optparse.OptionParser()
+    parser.add_option("-c", "--clean",
+                      action="store_true", dest="clean", default=False,
+                      help="Ignore the settings.yaml file if it exists and \
+                      prompt for new settings.")
+    
+    (options, args) = parser.parse_args()
+    
+    # Look for a settings.yaml file
+    settingsFile = 'settings.yaml'
+    if not options.clean and os.path.exists(settingsFile):
+        with open(settingsFile, 'r') as fh:
+            settings = yaml.load(fh)
+    # If we were asked to bypass, or don't have settings
+    else:
+        settings = {}
+        if os.name == "posix":
+            portPrompt = "Which port corresponds to your USB2Dynamixel? \n"
+            # Get a list of ports that mention USB
+            try:
+                possiblePorts = subprocess.check_output('ls /dev/*usb*',
+                                                        shell=True).split()
+            except subprocess.CalledProcessError:
+                sys.exit("USB2Dynamixel not found. Please connect one.")
+                
+            counter = 1
+            portCount = len(possiblePorts)
+            for port in possiblePorts:
+                portPrompt += "\t" + str(counter) + " - " + port + "\n"
+                counter += 1
+            portPrompt += "Enter Choice: "
+            portChoice = None
+            while not portChoice:                
+                portTest = raw_input(portPrompt)
+                portTest = validateInput(portTest, 1, portCount)
+                if portTest:
+                    portChoice = possiblePorts[portTest - 1]
+
+        else:
+            portPrompt = "Please enter the port name to which the USB2Dynamixel is connected: "
+            portChoice = raw_input(portPrompt)
+    
+        settings['port'] = portChoice
+        
+        # Baud rate
+        baudRate = None
+        while not baudRate:
+            brTest = raw_input("Enter baud rate [Default: 1000000 bps]:")
+            if not brTest:
+                baudRate = 1000000
+            else:
+                baudRate = validateInput(brTest, 9600, 1000000)
+                    
+        settings['baudRate'] = baudRate
+        
+        # Servo ID
+        highestServoId = None
+        while not highestServoId:
+            hsiTest = raw_input("Please enter the highest ID of the connected servos: ")
+            highestServoId = validateInput(hsiTest, 1, 255)
+        
+        settings['highestServoId'] = highestServoId
+        
+        # Save the output settings to a yaml file
+        with open(settingsFile, 'w') as fh:
+            yaml.dump(settings, fh)
+            print("Your settings have been saved to 'settings.yaml'. \nTo " +
+                   "change them in the future either edit that file or run " +
+                   "this example with -c.")
+    
+    main(settings)
