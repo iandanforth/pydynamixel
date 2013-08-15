5bd2c9dfa15820e8fc6063cc86ce93c4f4d7b0f5
Ian Danforth <iandanforth@gmail.com>
2013-08-15 14:19:57 -0700

    Add AX-S1 specific example script

---
+++ sensor-example.py
@@ -0,0 +1,133 @@
+import os
+import dynamixel
+import time
+import random
+import sys
+import subprocess
+import optparse
+import yaml
+
+"""
+EXAMPLE
+ 
+Move all attached servos randomly every 2 seconds and read back the position
+they end up in.
+"""
+
+def main(settings):
+
+    portName = settings['port']
+    baudRate = settings['baudRate']
+    
+    # Establish a serial connection to the dynamixel network.
+    # This usually requires a USB2Dynamixel
+    serial = dynamixel.SerialStream(port=portName, baudrate=baudRate, timeout=1)
+    net = dynamixel.DynamixelNetwork(serial)
+        
+    # Sensor is assumed to be at id 100
+    sensor = dynamixel.SensorModule(100, net)
+    
+    sensor.buzzer_time = 0
+    
+    # Pause to see if the value is kept
+    print "Set buzzer time value:", sensor.buzzer_time
+    sensor.buzzer_index = 19
+
+    time.sleep(1)
+
+    print "Re-Set buzzer time value:", sensor.buzzer_time
+    print "Set buzzer index value:", sensor.buzzer_index
+    
+    # Read values sensed from environment
+    print "Left IR \tCenter IR\tRight IR\tTemperature\tVoltage"
+    while True:
+        # Get our sensor values and time elapsed
+        lir = sensor.left_ir_sensor_value
+        cir = sensor.center_ir_sensor_value
+        rir = sensor.right_ir_sensor_value
+        temp = sensor.current_temperature
+        volts = sensor.current_voltage
+        print lir, '\t\t', cir, '\t\t', rir, '\t\t', temp, '\t\t', volts
+        time.sleep(1)
+        
+def validateInput(userInput, rangeMin, rangeMax):
+    '''
+    Returns valid user input or None
+    '''
+    try:
+        inTest = int(userInput)
+        if inTest < rangeMin or inTest > rangeMax:
+            print "ERROR: Value out of range [" + str(rangeMin) + '-' + str(rangeMax) + "]"
+            return None
+    except ValueError:
+        print("ERROR: Please enter an integer")
+        return None
+    
+    return inTest
+
+if __name__ == '__main__':
+    
+    parser = optparse.OptionParser()
+    parser.add_option("-c", "--clean",
+                      action="store_true", dest="clean", default=False,
+                      help="Ignore the settings.yaml file if it exists and \
+                      prompt for new settings.")
+    
+    (options, args) = parser.parse_args()
+    
+    # Look for a settings.yaml file
+    settingsFile = 'settings.yaml'
+    if not options.clean and os.path.exists(settingsFile):
+        with open(settingsFile, 'r') as fh:
+            settings = yaml.load(fh)
+    # If we were asked to bypass, or don't have settings
+    else:
+        settings = {}
+        if os.name == "posix":
+            portPrompt = "Which port corresponds to your USB2Dynamixel? \n"
+            # Get a list of ports that mention USB
+            try:
+                possiblePorts = subprocess.check_output('ls /dev/ | grep -i usb',
+                                                        shell=True).split()
+                possiblePorts = ['/dev/' + port for port in possiblePorts]
+            except subprocess.CalledProcessError:
+                sys.exit("USB2Dynamixel not found. Please connect one.")
+                
+            counter = 1
+            portCount = len(possiblePorts)
+            for port in possiblePorts:
+                portPrompt += "\t" + str(counter) + " - " + port + "\n"
+                counter += 1
+            portPrompt += "Enter Choice: "
+            portChoice = None
+            while not portChoice:                
+                portTest = raw_input(portPrompt)
+                portTest = validateInput(portTest, 1, portCount)
+                if portTest:
+                    portChoice = possiblePorts[portTest - 1]
+
+        else:
+            portPrompt = "Please enter the port name to which the USB2Dynamixel is connected: "
+            portChoice = raw_input(portPrompt)
+    
+        settings['port'] = portChoice
+        
+        # Baud rate
+        baudRate = None
+        while not baudRate:
+            brTest = raw_input("Enter baud rate [Default: 1000000 bps]:")
+            if not brTest:
+                baudRate = 1000000
+            else:
+                baudRate = validateInput(brTest, 9600, 1000000)
+                    
+        settings['baudRate'] = baudRate
+        
+        # Save the output settings to a yaml file
+        with open(settingsFile, 'w') as fh:
+            yaml.dump(settings, fh)
+            print("Your settings have been saved to 'settings.yaml'. \nTo " +
+                   "change them in the future either edit that file or run " +
+                   "this example with -c.")
+    
+    main(settings)
